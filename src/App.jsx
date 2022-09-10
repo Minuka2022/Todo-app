755cc78f27a0c1a9b61537b91300df921f18f485
Minuka2022 <metax.company.2022@gmail.com>
2022-09-10 10:11:05 -0700

    Todo-app(new)

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+function App() {
+
+
+    const [inputText , setInputText] = useState("");
+    const [items , setItems] = useState([]);
+  
+
+    function handleChange (event){
+        const newValue =event.target.value;
+        setInputText(newValue); 
+
+    }
+   
+    function addItem(){ 
+        setItems((preItems) => {
+            return[...preItems,inputText];
+        });
+
+    }
+
+
+
+
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={handleChange} type="text" />
+        <button onClick={addItem}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((todoItem) => <li>{todoItem}</li>)}
+          
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
